cde22ca0b5970b6e24e09b14606f0e16335c2d5b
Sathish Babu <sathishbabur@hotmail.com>
2020-02-19 12:25:57 +0530

    added html content inside default method for google domain verification

--- src/main/java/com/arasoftwares/gmailservice/GMailClientController.java
+++ src/main/java/com/arasoftwares/gmailservice/GMailClientController.java
@@ -14,7 +14,10 @@ public class GMailClientController {
     @GetMapping
     public String sayHi() {
         System.out.println("say hi method called");
-        return "Hi";
+        return "<html><head><meta name='google-site-verification' content='q3pmlPcQxSJgwGTwhopW4jx99ZMfUqrn3kYrf1PDSw8'/>"
+               +"<title>Gmail-Push Notification </title>"
+               +"</head><body><h1>Hello World!</h1>"
+               +"<p>Welcome to the Web App</body></html>";
     }
 
     @PostMapping("push")
